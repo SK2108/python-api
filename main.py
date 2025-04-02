9a7b4e361f44a60d67317cbb938337084a9cac11
SK2108 <85369151+SK2108@users.noreply.github.com>
2025-04-02 14:23:01 +0200

    Creating a new function for consuming the API

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 """Module providing requests"""
 import requests
 
-response = requests.get("https://fakestoreapi.com/products", timeout=5)
-print(response)
-print(response.text)
+
+def get_products():
+    """Create your function"""
+    url = "https://fakestoreapi.com/products"
+
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            products = response.json()
+            return products
+    except requests.exceptions.RequestException as e:
+        print('Your request has failed:', e)
+        return None
+
+
+def main():
+    """The main function"""
+    products = get_products()
+
+    if products:
+        print(products)
+    else:
+        print('Failed to get products from the API.')
+
+
+if __name__ == '__main__':
+    main()
